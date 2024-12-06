fbabecb1039ac6464ae8b756aa19c7ad824048e8
juling <julinger.qq.com>
2024-12-06 16:05:26 +0800

    fix typo

--- include/hz_downsampler.h
+++ include/hz_downsampler.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: juling julinger.qq.com
+ * @Date: 2024-09-13 10:27:51
+ * @LastEditors: juling julinger.qq.com
+ * @LastEditTime: 2024-12-06 16:03:31
+ * @FilePath: /perception_ws/src/perception_test/include/hz_downsampler.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #ifndef HZ_DOWNSAMPLER_H
 #define HZ_DOWNSAMPLER_H
 #include <chrono>
@@ -14,7 +22,7 @@ class HzDownSampler {
 
   ~HzDownSampler(){};
   // Returns true if this pulse should result in an sample.
-  bool Pluse();
+  bool Pulse();
 
  private:
   // The cycle time.
